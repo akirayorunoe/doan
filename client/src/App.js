f43fddf8a8a707da57cefb74d3d5917f4dc732e5
huypham1411 <54835897+huypham1411@users.noreply.github.com>
2020-06-28 12:53:32 +0700

    test lazy

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,39 @@
-import React,{useEffect} from "react";
-import Home from "./containers/Home";
+import React,{useEffect, Suspense, lazy} from "react";
+// import Home from "./containers/Home";
 import "./App.css";
-import Header from "./components/General/Header";
-import Nav from "./components/General/Nav";
+// import Nav from "./components/General/Nav";
+// import Footer from "./components/General/Footer";
+import Nav from './components/General/Nav';
+import Header from './components/General/Header';
+// import About from './containers/About';
+// import signUp from './containers/signUp';
+// import Policy from "./containers/Policy";
+// import Forgot_pass from "./containers/ForgetPass";
+// import Products from "./containers/Product";
+// import ProductInfo from "./components/Product/ProductInfo";
+// import Cart from "./components/General/Cart";
+// import SearchPage from "./containers/SearchPage";
+// import UserPage from "./containers/UserPage";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import "./fonts/Roboto-Regular.ttf";
-import About from "./containers/About";
-import signUp from "./containers/signUp";
-import Footer from "./components/General/Footer";
-import Policy from "./containers/Policy";
-import Forgot_pass from "./containers/ForgetPass";
-import Products from "./containers/Product";
-import ProductInfo from "./components/Product/ProductInfo";
-import Cart from "./components/General/Cart";
-import SearchPage from "./containers/SearchPage";
 import {useDispatch} from 'react-redux';
 import {usrLogin} from './action/user-login'
 import axios from 'axios';
-import UserPage from "./containers/UserPage";
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+// import MessengerCustomerChat from 'react-messenger-customer-chat';
+
+const Home = lazy (()=>import('./containers/Home'));
+// const Nav = lazy (()=>import('./components/General/Nav'));
+// const Header = lazy(()=>import('./components/General/Header'));
+const About = lazy(()=> import('./containers/About')) ;
+const signUp = lazy(()=> import('./containers/signUp'));
+const Footer = lazy(()=> import('./components/General/Footer'));
+const Policy = lazy(()=> import ("./containers/Policy"));
+const Forgot_pass = lazy(()=> import("./containers/ForgetPass"));
+const Products = lazy(()=>import("./containers/Product"));
+const ProductInfo = lazy(()=>import( "./components/Product/ProductInfo"));
+const Cart = lazy(()=>import("./components/General/Cart"));
+const SearchPage = lazy(()=>import ("./containers/SearchPage"));
+const UserPage = lazy(()=>import ("./containers/UserPage"));
+const MessengerCustomerChat = lazy(()=>import('react-messenger-customer-chat'));
 
 function App() {
   const dispatch=useDispatch();
@@ -34,10 +49,14 @@ fetchUser();}
 }
   ,[]);
   return (
+    
     <Router>
       <div className="App">
         <Header />
-        <Nav />
+        
+          <Nav />
+        
+        <Suspense fallback='loading....'>
         <Switch>
           <Route path="/" exact component={Home}/>
           <Route path ="/Policy" component={Policy}/>
@@ -50,13 +69,18 @@ fetchUser();}
           <Route path="/Search" component={SearchPage}/>
           <Route path="/User" component={UserPage}/>
       </Switch>
+      </Suspense>
       <div>
+      <Suspense fallback='loading...'>
       <MessengerCustomerChat
         pageId="100367225056687"
         appId="183057163147995"
       />
+      </Suspense>
       </div>
+      <Suspense fallback={<div/>}>
           <Footer />
+      </Suspense>
     </div>
   </Router>
   );
--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import '../styles/containers/Home.css';
 import BgHeader from '../components/Home/BgHeader';
 // import CardList from '../components/Home/cardList/Cardlist';
 import CircleWrapper from '../components/Home/CircleWrapper';
 import HotBar from '../components/Home/HotBar';
-import HotList from '../components/Home/cardList/HotList';
+// import HotList from '../components/Home/cardList/HotList';
+
+const HotList =lazy(()=>import ('../components/Home/cardList/HotList'));
 const Home =()=>{
         return (
            <div className="home">
             <BgHeader/>
             <CircleWrapper />
             <HotBar/>
-            <HotList/>
+            <Suspense fallback={<div/>}> 
+                <HotList/>
+            </Suspense>
             {/* <CardList/> */}
         </div>
         );
