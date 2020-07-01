d643daa5d80ed503b1f6b6ae05d5a9f14b24b7bf
huypham1411 <54835897+huypham1411@users.noreply.github.com>
2020-07-01 21:26:33 +0700

    test performance

--- client/src/components/Home/BgHeader.js
+++ client/src/components/Home/BgHeader.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from '../../assets/organic.jpg';
 import '../../styles/components/Home/BgHeader.css';
-const BgHeader=(props)=>{
+const BgHeader=()=>{
     return(
       <img className="header-image" src={Image} alt="header-img"/> 
     );
