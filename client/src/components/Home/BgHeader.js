044e09e80c6c901df3ddfb0edbfed95a3c39e345
huypham1411 <54835897+huypham1411@users.noreply.github.com>
2020-07-02 19:33:16 +0700

    fix searchPage and Res circle

--- client/src/components/Home/BgHeader.js
+++ client/src/components/Home/BgHeader.js
@@ -3,7 +3,7 @@ import Image from '../../assets/organic.jpg';
 import '../../styles/components/Home/BgHeader.css';
 const BgHeader=()=>{
     return(
-      <img className="header-image" src={Image} alt="header-img"/> 
+      <img className="header-image" src='https://res.cloudinary.com/testuncleveggieimg/image/upload/v1593613729/organic_u0beqw.webp' alt="header-img"/> 
     );
 }
 export default BgHeader;
